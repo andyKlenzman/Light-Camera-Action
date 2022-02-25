a20c4b7df53a296d658dff9746dbfcf072d7fc27
andyKlenzman <92612316+andyKlenzman@users.noreply.github.com>
2022-02-24 19:29:01 -0600

    Initial Commit

---
+++ Light Camera ActionUITests/Light_Camera_ActionUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Light_Camera_ActionUITestsLaunchTests.swift
+//  Light Camera ActionUITests
+//
+//  Created by Andy on 2/24/22.
+//
+
+import XCTest
+
+class Light_Camera_ActionUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
